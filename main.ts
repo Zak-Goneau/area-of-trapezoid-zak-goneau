05f0de3dc217cf0086cf216d2fcd7705bd3ceed7
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2022-12-06 17:52:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,6 @@
-game.splash("This program calcualtes the area of a trapezoid")
+game.splash("This program calculates the area of a trapezoid in centimeters")
 let base1 = game.askForNumber("What is the length of the first base (cm) ?")
+let base2 = game.askForNumber("What is the length of the second base (cm) ?")
+let height = game.askForNumber("What is the height of the trapezoid (cm) ?")
+let area = (base1 + base2) / 2 * height
+game.splash("The area of a trapezoid with the base1 of " + base1 + "cm, base2 of " + base2 + "cm, and a height of " + height + "cm, will be " + area + "cm squared")
